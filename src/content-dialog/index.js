2a51003b5dedd7a1cb25477fab6f0c9079154d61
0x00a <paolo@async.ly>
2018-08-09 15:24:56 +0200

    background

--- src/content-dialog/index.js
+++ src/content-dialog/index.js
@@ -101,7 +101,10 @@ class ContentDialog extends Tonic { /* global Tonic */
     // close button
     const close = document.createElement('div')
     close.className = 'close'
-    close.style.backgroundImage = `url("${this.props.closeIcon(color)}")`
+
+    const computedStyles = window.getComputedStyle(this.root)
+    const colorAttr = color || computedStyles.getPropertyValue('--medium')
+    close.style.backgroundImage = `url("${this.props.closeIcon(colorAttr)}")`
 
     // append everything
     wrapper.appendChild(dialog)
@@ -115,8 +118,6 @@ class ContentDialog extends Tonic { /* global Tonic */
 ContentDialog.svg = {}
 
 ContentDialog.svg.closeIcon = (color) => {
-  color = color || 'red'
-
   const svg = `
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
       <path fill="${color}" d="M80.7,22.6l-3.5-3.5c-0.1-0.1-0.3-0.1-0.4,0L50,45.9L23.2,19.1c-0.1-0.1-0.3-0.1-0.4,0l-3.5,3.5c-0.1,0.1-0.1,0.3,0,0.4l26.8,26.8L19.3,76.6c-0.1,0.1-0.1,0.3,0,0.4l3.5,3.5c0,0,0.1,0.1,0.2,0.1s0.1,0,0.2-0.1L50,53.6l25.9,25.9c0.1,0.1,0.3,0.1,0.4,0l3.5-3.5c0.1-0.1,0.1-0.3,0-0.4L53.9,49.8l26.8-26.8C80.8,22.8,80.8,22.7,80.7,22.6z"/>

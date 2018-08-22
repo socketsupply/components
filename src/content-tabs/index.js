66180ff41d5a212ac051ad5161692972c4d5d380
0x00a <paolo@async.ly>
2018-08-22 15:39:04 +0200

    update components

--- src/content-tabs/index.js
+++ src/content-tabs/index.js
@@ -4,7 +4,35 @@ class ContentTabs extends Tonic { /* global Tonic */
   }
 
   style () {
-    return `%style%`
+    return {
+      'content-tabs': {
+        display: 'block'
+      },
+
+      '[data-tab-name]:not([data-tab-group])': {
+        userSelect: 'none',
+        fontFamily: 'var(--subheader)',
+        fontSize: '14px',
+        borderBottom: '1px solid transparent',
+        marginRight: '8px'
+      },
+
+      '.tonic--selected': {
+        color: 'var(--accent)',
+        borderBottom: '1px solid var(--accent)'
+      },
+
+      '[data-tab-group]': {
+        marginTop: '15px',
+        display: 'none',
+        borderTop: '1px solid var(--border)',
+        paddingTop: '15px'
+      },
+
+      '.tonic--show': {
+        display: 'block'
+      }
+    }
   }
 
   qs (s, p) {

1bcc4d7a15361c4decf8566f83fff94dab56caa3
aprileelcich <aprile.elcich@gmail.com>
2018-11-08 12:44:54 -0800

    help

---
+++ test/src/dialog/index.js
@@ -0,0 +1,34 @@
+const Tonic = require('@conductorlab/tonic')
+const components = require('../..')
+
+components(Tonic)
+
+class TonicDialog extends Tonic.Dialog {
+  click (e) {
+    if (!Tonic.match(e.target, '#update')) return
+
+    this.reRender(props => ({
+      ...props,
+      message: `Date stamp ${Date.now()}`
+    }))
+  }
+
+  render () {
+    return `
+      <header>Dialog</header>
+      <main>
+        ${this.props.message}
+      </main>
+      <footer>
+        <tonic-button id="update">Update</tonic-button>
+      </footer>
+    `
+  }
+}
+
+Tonic.add(TonicDialog)
+
+const link = document.getElementById('dialog-default-button')
+const dialog = document.getElementById('dialog-default')
+
+link.addEventListener('click', e => dialog.show())

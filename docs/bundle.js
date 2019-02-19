7d56713a7cecedda5b6e2221bfec4e53a54adaa3
aprileelcich <aprile.elcich@gmail.com>
2019-02-19 14:42:56 +0700

    wip tests

--- docs/bundle.js
+++ docs/bundle.js
@@ -2998,7 +2998,7 @@ class TonicSelect extends Tonic { /* global Tonic */
       }
 
       tonic-select select:not([multiple]) {
-        padding: 10px 20px 10px 10px;
+        padding: 10px 30px 10px 10px;
       }
 
       tonic-select select[disabled] {

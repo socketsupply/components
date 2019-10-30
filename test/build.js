5e35391d94807b447db5532e927e909ed1f08653
heapwolf <paolo@async.ly>
2019-10-30 12:56:54 +0100

    rebuild docs

--- test/build.js
+++ test/build.js
@@ -27,15 +27,15 @@ const index = `
       <meta http-equiv="Content-Type" charset="utf-8" content="text/html">
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1 user-scalable=no">
       <meta name="description" content="Component Based Architecture">
-      <meta property="og:site_name", content="ConductorLab">
+      <meta property="og:site_name", content="Operator Tool Co.">
       <meta property="og:title", content="Tonic Components">
       <meta property="og:description" content="Component Based Architecture">
       <meta property="og:type", content="website">
-      <meta property="og:url", content="https://hxoht.github.io/components">
-      <meta property="og:image", content="https://hxoht.github.io/components/tonic_preview.png">
+      <meta property="og:url", content="https://optoolco.github.io/components">
+      <meta property="og:image", content="https://optoolco.github.io/components/tonic_preview.png">
       <meta name="twitter:card", content="summary">
-      <meta name="twitter:site", content="@conductorlab">
-      <meta name="twitter:image", content="https://hxoht.github.io/components/tonic_preview.png">
+      <meta name="twitter:site", content="@optoolco">
+      <meta name="twitter:image", content="https://optoolco.github.io/components/tonic_preview.png">
       <meta
         http-equiv="Content-Security-Policy"
         content="

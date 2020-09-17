6b2726975dfb29dc551ba50d9fc77ae34527b210
heapwolf <paolo@async.ly>
2020-09-17 18:41:18 +0200

    reduce coplexity of dialog

--- dialog/index.js
+++ dialog/index.js
@@ -4,13 +4,33 @@ class Dialog extends Tonic {
   constructor () {
     super()
 
-    this.addEventListener('click', e => {
-      const el = Tonic.match(e.target, '.tonic--dialog-close')
-      if (el) this.hide()
+    this.classList.add('tonic--dialog')
 
-      const overlay = e.target.matches('.tonic--dialog-overlay')
-      if (overlay && !this.props.preventOverlayClick) this.hide()
-    })
+    if (!document.querySelector('.tonic--dialog--overlay')) {
+      const div = document.createElement('div')
+      div.classList = 'tonic--dialog--overlay'
+      div.textContent = ' '
+      document.body.appendChild(div)
+    }
+
+    this.closeIcon = document.createElement('div')
+    this.closeIcon.className = 'tonic--close'
+    this.closeIcon.addEventListener('click', e => this.hide())
+
+    const svgns = 'http://www.w3.org/2000/svg'
+    const xlinkns = 'http://www.w3.org/1999/xlink'
+    const svg = document.createElementNS(svgns, 'svg')
+    const use = document.createElementNS(svgns, 'use')
+
+    this.closeIcon.appendChild(svg)
+    svg.appendChild(use)
+
+    use.setAttributeNS(xlinkns, 'href', '#close')
+    use.setAttributeNS(xlinkns, 'xlink:href', '#close')
+
+    const iconColor = 'var(--tonic-primary, #333)'
+    use.setAttribute('color', iconColor)
+    use.setAttribute('fill', iconColor)
   }
 
   defaults () {
@@ -18,68 +38,92 @@ class Dialog extends Tonic {
       width: '450px',
       height: 'auto',
       overlay: true,
-      backgroundColor: 'rgba(0,0,0,0.5)'
+      backgroundColor: 'rgba(0, 0, 0, 0.5)'
     }
+  }
+
+  getZIndex () {
+    return Array.from(document.querySelectorAll('body *'))
+      .map(elt => parseFloat(window.getComputedStyle(elt).zIndex))
+      .reduce((z, highest = Number.MIN_SAFE_INTEGER) =>
+        isNaN(z) || z < highest ? highest : z
+      )
   }
 
   static stylesheet () {
     return `
-      .tonic--dialog .tonic--dialog--wrapper {
+      .tonic--dialog {
+        box-shadow: 0px 4px 16px 3px rgba(0, 0, 0, 0.3);
+        background: var(--tonic-window);
+        border-radius: 6px;
+        position: fixed;
+        overflow: hidden;
+        top: 50%;
+        left: 50%;
+        z-index: -1;
+        opacity: 0;
+        transition: z-index .3s;
+        transform: translate(-50%, -50%) scale(1.22);
+        will-change: transform;
+      }
+
+      .tonic--dialog.tonic--show {
+        transform: translate(-50%, -50%) scale(1);
+        opacity: 1;
+        animation-duration: .3s;
+        animation-name: tonic--dialog--show;
+        animation-delay: 0s;
+      }
+
+      .tonic--dialog.tonic--hide {
+        transform: translate(-50%, -50%) scale(1.22);
+        opacity: 0;
+        animation-duration: .1s;
+        animation-name: tonic--dialog--hide;
+        animation-delay: 0s;
+      }
+
+      @keyframes tonic--dialog--show {
+        from {
+          transform: translate(-50%, -50%) scale(1.22);
+          opacity: 0;
+        }
+
+        to {
+          opacity: 1;
+          transform: translate(-50%, -50%) scale(1);
+        }
+      }
+
+      @keyframes tonic--dialog--hide {
+        from {
+          transform: translate(-50%, -50%) scale(1);
+          opacity: 1;
+        }
+
+        to {
+          opacity: 0;
+          transform: translate(-50%, -50%) scale(1.22);
+        }
+      }
+
+      .tonic--dialog--overlay {
         position: fixed;
         top: 0;
         left: 0;
         right: 0;
         bottom: 0;
-        display: flex;
-        z-index: 100;
-        visibility: hidden;
-        transition: visibility 0s ease 0.2s;
+        opacity: 0;
+        z-index: -1;
+        transition: all 0.3s;
+        background: var(--tonic-overlay);
       }
 
-      .tonic--dialog .tonic--dialog--wrapper.tonic--show {
-        visibility: visible;
-        transition: visibility 0s ease 0s;
-      }
-
-      .tonic--dialog .tonic--dialog--wrapper.tonic--show .tonic--overlay {
+      .tonic--dialog--overlay.tonic--show {
         opacity: 1;
       }
 
-      .tonic--dialog .tonic--dialog--wrapper.tonic--show .tonic--dialog--content {
-        color: var(--tonic-primary, #333);
-        opacity: 1;
-        -webkit-transform: scale(1);
-        -ms-transform: scale(1);
-        transform: scale(1);
-      }
-
-      .tonic--dialog .tonic--overlay {
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        opacity: 0;
-        transition: opacity 0.2s ease-in-out;
-      }
-
-      .tonic--dialog .tonic--dialog--content {
-        min-width: 350px;
-        min-height: 250px;
-        height: auto;
-        width: auto;
-        margin: auto;
-        position: relative;
-        background-color: var(--tonic-window, #fff);
-        z-index: 1;
-        opacity: 0;
-        -webkit-transform: scale(0.8);
-        -ms-transform: scale(0.8);
-        transform: scale(0.8);
-        transition: all 0.2s ease-in-out;
-      }
-
-      .tonic--dialog .tonic--dialog--content > .tonic--close {
+      .tonic--dialog > .tonic--close {
         width: 25px;
         height: 25px;
         position: absolute;
@@ -88,42 +132,54 @@ class Dialog extends Tonic {
         cursor: pointer;
       }
 
-      .tonic--dialog .tonic--close svg {
+      .tonic--dialog > .tonic--close svg {
         width: inherit;
         height: inherit;
       }
     `
   }
 
+  updated () {
+    window.requestAnimationFrame(() => {
+      this.appendChild(this.closeIcon)
+    })
+  }
+
   show () {
-    const that = this
+    const z = this.getZIndex()
+
+    const overlay = document.querySelector('.tonic--dialog--overlay')
+    overlay.classList.add('tonic--show')
+    this.style.zIndex = z + 100
+    overlay.style.zIndex = z
 
     return new Promise(resolve => {
-      window.requestAnimationFrame(() => {
-        const node = this.querySelector('.tonic--dialog--wrapper')
-        const content = this.querySelector('.tonic--dialog--content')
-        node.classList.add('tonic--show')
-        content.addEventListener('transitionend', resolve, { once: true })
+      this.style.width = this.props.width
+      this.style.height = this.props.height
+      this.addEventListener('animationend', resolve, { once: true })
+      this.classList.remove('tonic--hide')
+      this.classList.add('tonic--show')
 
-        this._escapeHandler = e => {
-          if (e.keyCode === 27) that.hide()
-        }
+      this._escapeHandler = e => {
+        if (e.keyCode === 27) this.hide()
+      }
 
-        document.addEventListener('keyup', that._escapeHandler)
-      })
+      document.addEventListener('keyup', this._escapeHandler)
     })
   }
 
   hide () {
+    const overlay = document.querySelector('.tonic--dialog--overlay')
+    overlay.classList.remove('tonic--show')
+    overlay.style.zIndex = -1
     const that = this
 
     return new Promise(resolve => {
-      window.requestAnimationFrame(() => {
-        const node = this.querySelector('.tonic--dialog--wrapper')
-        node.classList.remove('tonic--show')
-        node.addEventListener('transitionend', resolve, { once: true })
-        document.removeEventListener('keyup', that._escapeHandler)
-      })
+      this.addEventListener('animationend', resolve, { once: true })
+      this.classList.remove('tonic--show')
+      this.classList.add('tonic--hide')
+      this.style.zIndex = -1
+      document.removeEventListener('keyup', that._escapeHandler)
     })
   }
 
@@ -147,103 +203,6 @@ class Dialog extends Tonic {
         that.addEventListener(eventName, listener)
       }
     }
-  }
-
-  async * wrap () {
-    const {
-      width,
-      height,
-      overlay,
-      theme,
-      color,
-      backgroundColor
-    } = this.props
-
-    this.classList.add('tonic--dialog')
-
-    const wrapper = document.createElement('div')
-
-    const isOpen = !!this.querySelector('.tonic--dialog--wrapper.tonic--show')
-    wrapper.className = isOpen ? 'tonic--dialog--wrapper tonic--show' : 'tonic--dialog--wrapper'
-
-    if (theme) this.classList.add(`tonic--theme--${theme}`)
-
-    if (overlay !== 'false') {
-      const overlayElement = document.createElement('div')
-      overlayElement.className = 'tonic--overlay tonic--dialog-overlay'
-      overlayElement.style.backgroundColor = backgroundColor
-      wrapper.appendChild(overlayElement)
-    }
-
-    const dialog = document.createElement('div')
-    dialog.className = 'tonic--dialog--content'
-    if (width) dialog.style.width = width
-    if (height) dialog.style.height = height
-
-    // create template
-    const closeIcon = document.createElement('div')
-    closeIcon.className = 'tonic--close tonic--dialog-close'
-
-    // create SVG
-    const svgns = 'http://www.w3.org/2000/svg'
-    const xlinkns = 'http://www.w3.org/1999/xlink'
-    const svg = document.createElementNS(svgns, 'svg')
-    const use = document.createElementNS(svgns, 'use')
-
-    closeIcon.appendChild(svg)
-    svg.appendChild(use)
-
-    const iconColor = color || 'var(--tonic-primary, #333)'
-
-    use.setAttributeNS(xlinkns, 'href', '#close')
-    use.setAttributeNS(xlinkns, 'xlink:href', '#close')
-    use.setAttribute('color', iconColor)
-    use.setAttribute('fill', iconColor)
-
-    wrapper.appendChild(dialog)
-    const contentContainer = document.createElement('div')
-    contentContainer.className = 'tonic--dialog--content-container'
-    dialog.appendChild(contentContainer)
-    dialog.appendChild(closeIcon)
-
-    yield wrapper
-
-    const setContent = content => {
-      if (!content) return
-
-      const isRaw = content.isTonicUnsafeString ||
-        content.isTonicRaw || content.isTonicTemplate
-
-      if (typeof content === 'string') {
-        contentContainer.innerHTML = content
-      } else if (isRaw) {
-        contentContainer.innerHTML = content.rawText
-      } else {
-        [...content.childNodes].forEach(el => contentContainer.appendChild(el))
-      }
-    }
-
-    if (this.wrapped instanceof Tonic.AsyncFunction) {
-      setContent(await this.wrapped() || '')
-      return wrapper
-    } else if (this.wrapped instanceof Tonic.AsyncFunctionGenerator) {
-      const itr = this.wrapped()
-      while (true) {
-        const { value, done } = await itr.next()
-        setContent(value)
-
-        if (done) {
-          return wrapper
-        }
-
-        yield wrapper
-      }
-    } else if (this.wrapped instanceof Function) {
-      setContent(this.wrapped() || '')
-      return wrapper
-    }
-
-    return wrapper
   }
 }
 

1645364abccd3f4c6f42d7ff99789186c9c60bbd
Raynos <raynos2@gmail.com>
2021-11-23 09:28:47 -0500

    cleanup index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,8 @@
 import * as toaster from "./toaster"
 
-/**
- * Typescript has a bug where it cannot import JS files correctly
- * Sub classing them works for reasons unknown so just do it.
- */
-class CTonicToaster extends toaster.TonicToaster {}
-
 declare global {
   interface HTMLElementTagNameMap {
-    "tonic-toaster": CTonicToaster;
+    "tonic-toaster": toaster.TonicToaster;
   }
 }
 

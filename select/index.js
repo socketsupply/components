cb7ad463738daf53f6d06b5ee7546729eaa4eb0c
Raynos <raynos2@gmail.com>
2022-05-16 16:56:35 +0200

    fix tonic--invalid overlap for select

--- select/index.js
+++ select/index.js
@@ -113,7 +113,7 @@ class TonicSelect extends Tonic {
 
       tonic-select[edited] select[invalid] ~ .tonic--invalid,
       tonic-select[edited] select:invalid ~ .tonic--invalid {
-        transform: translateY(0);
+        transform: translate(-50%, 0);
         visibility: visible;
         opacity: 1;
         transition: opacity 0.2s ease, transform 0.2s ease, visibility 1s ease 0s;
@@ -129,8 +129,8 @@ class TonicSelect extends Tonic {
         margin-bottom: 13px;
         position: absolute;
         bottom: 100%;
-        left: 0;
-        right: 0;
+        left: 50%;
+        width: fit-content;
         transform: translateY(-10px);
         transition: opacity 0.2s ease, transform 0.2s ease, visibility 0s ease 1s;
         visibility: hidden;
